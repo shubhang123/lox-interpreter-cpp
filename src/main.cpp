774f668661be5f6a380422143e56f25e55091cb9
shubhang <shubhang.chakrawarty@gmail.com>
2025-02-24 16:51:29 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,6 @@
 #include "../include/parser.h"
 #include "../include/utils.h"
 
-// Helper function: Reads the entire file into a string.
-std::string readFileContents(const std::string &filename) {
-    std::ifstream file(filename);
-    if (!file) {
-        std::cerr << "Error reading file: " << filename << std::endl;
-        std::exit(1);
-    }
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    return buffer.str();
-}
-
-// Helper function: Prints usage information and exits.
-void printUsageAndExit(const std::string &programName) {
-    std::cerr << "Usage: " << programName << " tokenize|parse <filename>" << std::endl;
-    std::exit(1);
-}
-
 int main(int argc, char *argv[]) {
     if (argc < 3)
         printUsageAndExit(argv[0]);
